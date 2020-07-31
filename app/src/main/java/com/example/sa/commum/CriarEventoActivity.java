b8950b489f69db1c339496827d37e34ca2fc160a
ediar <ediarts7@gmail.com>
2020-07-31 15:14:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sa/commum/CriarEventoActivity.java
@@ -0,0 +1,16 @@
+package com.example.sa.commum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sa.R;
+
+public class CriarEventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_evento);
+    }
+}
---
+++ app/src/main/java/com/example/sa/commum/PerfilActivity.java
@@ -0,0 +1,15 @@
+package com.example.sa.commum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sa.R;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
